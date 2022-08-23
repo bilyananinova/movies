f29cfeb3f914c3e1ceeaaa9a183a518cbac32955
Bilyana Ninova <bilyananinova@gmail.com>
2022-08-23 21:52:27 +0300

    add favicon

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 let express = require('express');
 let cookieParser = require('cookie-parser');
+let favicon = require('serve-favicon');
+let path = require('path');
 
 let app = express();
 
@@ -10,6 +12,7 @@ let auth = require('./middlewares/auth');
 require('./config/mongoose')(app);
 require('./config/express-hbs')(app);
 
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use('/static', express.static('public'));
 app.use(cookieParser());
 app.use(auth());
