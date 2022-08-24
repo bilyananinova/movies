c757cbb999b42f6e4a59b5d6da159933041d8e07
Bilyana Ninova <bilyananinova@gmail.com>
2022-08-24 19:36:27 +0300

    change env port

--- config/config.js
+++ config/config.js
@@ -8,12 +8,11 @@ let config = {
         DB_CONNECTION: 'mongodb://localhost:27017/movies'
     },
     'production': {
-        PORT: 80,
+        PORT: process.env.PORT || 80,
         SALT_ROUNDS: 10,
         SECRET: 'mySecret',
         COOKIE_NAME: 'USER_SESSION',
         DB_CONNECTION: 'mongodb+srv://admin:adminpassword@cluster0.mrpru.mongodb.net/moviesDb?retryWrites=true&w=majority'
     }
 }
-
 module.exports = config[process.env.NODE_ENV.trim()]
